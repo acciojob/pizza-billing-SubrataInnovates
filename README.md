050b719c753213286a34f65bd243a5a23556395e
Mondal <subrata.b.mondal@capgemini.com>
2024-02-13 11:52:11 +0530

    All function is implemented

--- README.md
+++ README.md
@@ -9,7 +9,10 @@ Extra Toppings For Veg Pizza = 70\
 Extra Toppings For Non-veg Pizza = 120\
 Paper bag Price = 20
 
-You are given three classes in your driver code. Main.java, Pizza.java and DeluxePizza.java. Your DeluxePizza class should extends Pizza class and it shouldn’t have any extra methods in DeluxePizza class, i.e. make sure it’s constructor takes care of extra cheese and toppings. You have to complete the skeleton code in order to make your app work.
+You are given three classes in your driver code. Main.java, Pizza.java and DeluxePizza.java. 
+Your DeluxePizza class should extends Pizza class and it shouldn’t have any extra methods in DeluxePizza class,
+i.e. make sure it’s constructor takes care of extra cheese and toppings. 
+You have to complete the skeleton code in order to make your app work.
 
 **Bill Structure Example:**
 
@@ -27,6 +30,7 @@ Extra Toppings Added: 120\
 Total Price: 600
 
 **Note:**\
-The customer might ask for extra cheese or toppings by invoking your method multiple times, but you have to make sure you don’t add it in the bill multiple times.\
+The customer might ask for extra cheese or toppings by invoking your method multiple times, 
+but you have to make sure you don’t add it in the bill multiple times.\
 Make sure you add extra cheese before the extra toppings if a customer asks for both and it reflect the same in the billing.\
 In the billing make sure the string for bill you generate has all components in a different line. (Don’t forget to add “\n” after each line of billing)
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,30 +5,66 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    private boolean extraCheeseAdded;
+    private boolean extraToppingsAdded;
+    private boolean takeawayAdded;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        this.price=isVeg ? 300:400;
     }
 
-    public int getPrice(){
+    public int getPrice()
+    {
         return this.price;
+
     }
 
     public void addExtraCheese(){
         // your code goes here
+        if(!extraCheeseAdded)
+        {
+            extraCheeseAdded=true;
+            this.price+=80;
+
+        }
+        System.out.println("Extra Cheese Added:"+this.price);
+
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!extraToppingsAdded)
+        {
+            extraToppingsAdded=true;
+            this.price+=isVeg?70:120;
+        }
+        System.out.println("Extra Toppings Added:"+price);
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!takeawayAdded)
+        {
+            takeawayAdded=true;
+            this.price+=20;
+        }
+
     }
 
     public String getBill(){
         // your code goes here
-        return this.bill;
+        StringBuilder bill=new StringBuilder();
+        bill.append("Base Price Of The Pizza: ").append(this.price).append("\n");
+        if(extraCheeseAdded)
+            bill.append("Extra Cheese Added: 80\n");
+        if(extraToppingsAdded)
+            bill.append("Extra Toppings Added: ").append(isVeg?70:120).append("\n");
+        if (takeawayAdded)
+            bill.append("Paperbag Added: 20\n");
+
+        bill.append("Total Price: ").append(getPrice()).append(("\n"));
+        return bill.toString();
     }
 }
